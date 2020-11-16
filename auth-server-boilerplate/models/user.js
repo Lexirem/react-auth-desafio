268f3e973cd514cf970f7cc0d8030fa3bf9bb421
Lexirem <me.vallribera@gmail.com>
2020-11-16 17:03:24 +0100

    server-boilerplate fet

--- auth-server-boilerplate/models/user.js
+++ auth-server-boilerplate/models/user.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Creamos el modelo User
+// el modelo User tiene que tener un username y un password, que tienen que ser strings. También deberán tener un timestamps.
+
+const userSchema = new Schema({
+    username: String,
+    password: String
+},{
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    },
+});
 
 const User = mongoose.model('User', userSchema);
 

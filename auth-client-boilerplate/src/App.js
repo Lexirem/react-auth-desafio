6b4a537caa71ec9a72d55ca1da13048daf763d0f
Lexirem <me.vallribera@gmail.com>
2020-11-16 18:36:52 +0100

    auth-client lib

--- auth-client-boilerplate/src/App.js
+++ auth-client-boilerplate/src/App.js
@@ -2,24 +2,33 @@ import React, { Component } from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
+import AuthProvider from './lib/AuthProvider';
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-//importamos Login
-//importamos Private
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import Private from "./pages/Private";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Navbar />
+      <AuthProvider>        {/*       <---  Envolvemos los componentes con AuthProvider       */}
 
-        <Switch>
+        <div className="container">
+          <Navbar />
+          <h1>Basic React Authentication</h1>
+          <Switch>
           {/*     Agregamos ruta para Login
-        Agregamos ruta para el SignUp */}
+              Agregamos ruta para el SignUp */}
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/" component={Home} />
+            <Route exact path='/private' component={Private} />
+          </Switch>  
+        </div>
 
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </div>
+      </AuthProvider>
     );
   }
 }
---
+++ auth-client-boilerplate/src/lib/AuthProvider.js
@@ -0,0 +1,62 @@
+import React from "react";
+import auth from "./auth-service";	// Importamos funciones para llamadas axios a la API
+const { Consumer, Provider } = React.createContext();
+
+// HOC para crear Consumer
+const withAuth = (WrappedComponent) => {};
+
+// Provider
+class AuthProvider extends React.Component {
+  state = { isLoggedin: false, user: null, isLoading: true };
+
+  componentDidMount() {
+    auth.me()
+    //seteamos el estado a: isLoggedin: true, user: con el usuario conectado y isLoading en false 
+    .then((user) => this.setState({ isLoggedin: true, user: user, isLoading: false }))
+    //seteamos el estado a: isLoggedin: false, user: null, isLoading: false 
+    .catch((err) => this.setState({ isLoggedin: false, user: null, isLoading: false }));
+  }
+
+  signup = (user) => {
+    //desestructuramos username y password de user  
+    const { username, password } = user;
+    
+    auth.signup({ username, password })
+      .then((user) => this.setState({ isLoggedin: true, user: user }))
+      .catch(({response}) => this.setState({ message: response.data.statusMessage}));
+  };
+
+  login = (user) => {
+    const { username, password } = user;
+
+    auth.login({ username, password })
+      .then((user) => this.setState({ isLoggedin: true, user: user }))
+      .catch((err) => console.log(err));
+  };
+	
+  logout = () => {
+    auth.logout()
+      .then(() => this.setState({ isLoggedin: false, user: null }))
+      .catch((err) => console.log(err));
+  };
+	
+  render() {
+    //desestructutamos isLoading, isLoggedin y user del estado
+    const { isLoading, isLoggedin, user } = this.state;
+    // desestructuramos login, logout, signup
+    const { login, logout, signup } = this;
+    
+    return (
+      isLoading ? 
+      <div>Loading</div> 
+      :
+      (<Provider value={{ isLoggedin, user, login, logout, signup}} >
+         {this.props.children}
+      </Provider>)
+    )	/*<Provider> "value={}" datos que estarán disponibles para todos los componentes <Consumer> */
+  }
+}
+
+export { Consumer, withAuth };		//  <--	RECUERDA EXPORTAR  ! ! !
+
+export default AuthProvider;		//	<--	RECUERDA EXPORTAR  ! ! !
